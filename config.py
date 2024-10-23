ff8b5adbab11d6275774c80f525c1cfad5bd8782
monouj <diego32442@ephemeraleden.cloud>
2024-10-23 11:45:54 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ import os
 class Config(object):
 
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6134757239:AAEwejjgkvEN6qLwx8IlMiX7AkL8_VEnMtI")
 
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", "15257211"))
+    API_HASH = os.environ.get("API_HASH", "8b7cf73ce577720d74a213bbb98f4104")
     # Get these values from my.telegram.org
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1350212613").split())
 
     # Ban Unwanted Members..
     BANNED_USERS = []
@@ -26,7 +26,7 @@ class Config(object):
     CHUNK_SIZE = 128
 
     # Database url
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://AMMAAAAA:AMMAAAAA@cluster0.mi7ldio.mongodb.net/?retryWrites=true&w=majority")
 
     # Update channel for Force Subscribe, add bot as admin in the channel.
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
